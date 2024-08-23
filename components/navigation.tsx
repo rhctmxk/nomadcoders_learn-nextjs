f87fddf131a8cb9345106637a1afa1de976eec64
Jihyeon <annawlgus1004@gmail.com>
2024-08-23 10:43:15 +0900

    4.2 Movie Styles

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {useState} from "react";
+import { usePathname } from "next/navigation";
 import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
     const path = usePathname();
-    const [count, setCount] = useState(0);
-
     return (
         <nav className={styles.nav}>
-            <ul className={styles.list}>
-                <li><Link href="/">Home</Link> {path === "/" ? "🔥" : ""}</li>
-                <li><Link href="about-us">About Us!</Link> {path === "/about-us" ? "🔥" : ""}</li>
+            <ul>
                 <li>
-                    <button onClick={() => setCount((c) => c+1)}>{count}</button>
+                    <Link href="/">Home</Link> {path === "/" ? "🔥" : ""}
+                </li>
+                <li>
+                    <Link href="/about-us">About Us</Link>
+                    {path === "/about-us" ? "🔥" : ""}
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
