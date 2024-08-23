75f08f89a3b661f6c55720a6c7541a6757046c57
Jihyeon <annawlgus1004@gmail.com>
2024-08-23 11:27:35 +0900

    4.4 Dynamic Metadata

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,16 +1,23 @@
 import { API_URL } from "../../../(home)/page";
 import MovieVideos from "../../../../components/movie-videos";
 import MovieInfo from "../../../../components/movie-info";
+import { getMovie } from "../../../../components/movie-info";
 import {Suspense} from "react";
 
-export default async function MovieDetail({
-    params: { id },
-} : {
-    params: { id: string };
-}) {
+interface IParams {
+    params: {id: string};
+}
+
+export async function generateMetadata({params: {id}} : IParams) {
+    const movie = await getMovie(id);
+    return {
+        title: movie.title,
+    };
+}
+
+export default async function MovieDetailPage({params: { id }} : IParams) {
     return (
         <div>
-            <h3>Movie Detail Page</h3>
             <Suspense fallback={<h1>Loading movie info </h1>}>
                 <MovieInfo id={id} />
             </Suspense>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,6 +1,7 @@
+import styles from "../styles/movie-info.module.css";
 import {API_URL} from "../app/(home)/page";
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
@@ -8,6 +9,14 @@ async function getMovie(id: string) {
 export default async function MovieInfo({id} : {id: string}) {
     const movie = await getMovie(id);
     return(
-        <h6>{JSON.stringify(movie)}</h6>
+        <div className={styles.container}>
+            <img className={styles.poster} src={movie.poster_path} />
+            <div className={styles.info}>
+                <h1 className={styles.title}>{movie.title}</h1>
+                <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
+                <p>{movie.overview}</p>
+                <a href={movie.homepage} target={"_blank"}>Hompage &rarr;</a>
+            </div>
+        </div>
     )
 }
